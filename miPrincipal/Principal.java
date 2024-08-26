a6a65089ccceb587d31951a7a76e05128c36d97b
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-08-26 16:04:09 +0000

    Actualizacion clase Rational

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -8,8 +8,8 @@ public class Principal {
     public static void main(String[] args) {
         //System.out.println(new Principal().getGreeting());
         //Probar mi clase Rational
-        Rational r1 = new Rational(1,2);
-        Rational r2 = new Rational(1,2);
+        Rational r1 = new Rational(5,3);
+        Rational r2 = new Rational(2,3);
         Rational r3 = new Rational();
         r3 = r1.add(r1, r2);
         System.out.println("Resultado de suma:"+r3);
