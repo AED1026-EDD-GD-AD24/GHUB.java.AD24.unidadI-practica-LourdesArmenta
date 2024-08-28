dfb631c9668605e5f3d01c1d83b8182f1ca8f685
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-08-28 16:00:41 +0000

    Actualizacion clase Conjunto

--- miPrincipal/Conjunto.java
+++ miPrincipal/Conjunto.java
@@ -32,8 +32,71 @@ public class Conjunto {
 
     //añadir un elemento si no esta en el conjunto
     public void añadir(Object elemento){
+        if ( !pertenece(elemento)){
+            /*
+             * Verificar si hay posiciones libres
+             * en caso contrario ampliar el conjunto
+             */
+            if(cardinal == capacidad){
+                Object nuevoCto[];
+                nuevoCto = new Object[capacidad+M];
+                for (int k=0;k<capacidad;k++){
+                    nuevoCto[k]= cto[k];
+                }
+                capacidad = capacidad+ M;
+                cto = nuevoCto;
+
+            }
+            cto[cardinal++]=elemento;
+            
+        }
 
     }
+    //quita el elemento del comjunto
+    public void retirar(Object elemento){
+        if(pertenece(elemento)){
+            //localizar el elemento a retirar
+            int k=0;
+            while(!cto[k].equals(elemento))
+                k++;
+            /*
+             * desde el elemento k hasta la ultima
+             * posicion mover los elementos
+             * a la izquierda
+             */
+            for(;k<cardinal;k++){
+                cto[k]=cto[k+1];
+
+            }
+            cardinal--; //cardinal = cardinal -1;
+
+        }
+    }
+
+    //devuelva le número de elementos
+    public int cardinal(){
+        return this.cardinal;
+    }
+
+    //Operacion de union de dos conjuntos
+    public Conjunto union(Conjunto c2){
+        Conjunto u = new Conjunto();
+        //primero copio el primer conjunto de a union
+        for(int k=0; k<cardinal;k++){
+            u.cto[k]=this.cto[k];
+
+        }
+        u.cardinal=this.cardinal;
+        //añadir los elementos de c2 no incluidos
+        for (int k=0;k<c2.cardinal;k++){
+            u.añadir(c2.cto[k]);
+        }
+        return u;
+
+    }
+
+
+
 
     
 }
